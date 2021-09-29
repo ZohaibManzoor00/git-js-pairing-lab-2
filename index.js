3b0a9646c2dfce7e9108ebf53fb50a97fca97f79
Zohaib <zohaibmanzoor1700@gmail.com>
2021-09-29 18:44:57 +0000

    Driver zo

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 let OneHundredToFive = () => { for( let i = 100; i >= 5; i--) console.log(i) }
 
-OneHundredToFive()
+//OneHundredToFive()
 
 
 let absoluteValue = (num) => Math.abs(num)
-console.log(absoluteValue("hello"))
+//console.log(absoluteValue("hello"))
+
+let multiplesOfFourOrSeven = () => {
+    for(let i = 1; i <= 100; i++){
+        if(i % 4 === 0 || i % 7 === 0){
+            console.log(i)
+        }
+    }
+}
+
+multiplesOfFourOrSeven()
