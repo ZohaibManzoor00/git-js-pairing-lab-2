4504821a1f9d697222506262e78f72a3fcf85866
Zohaib <zohaibmanzoor1700@gmail.com>
2021-09-29 18:14:40 +0000

    Driver Zo

---
+++ index.js
@@ -0,0 +1,10 @@
+let OneHundredToFive = () => { for( let i = 100; i >= 5; i--) console.log(i) }
+
+//OneHundredToFive()
+
+function sum(x,y){
+    let value = x +y
+    return value;
+}
+
+sum(2, 4)
